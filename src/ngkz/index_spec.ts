0ceba28444d7b1a61807a979cac51e16384f6ecf
Gleison de Almeida <gleisonsubzerokz@gmail.com>
2022-04-10 22:46:25 -0300

    🧪 test: add cases for path mapping

--- src/ngkz/index_spec.ts
+++ src/ngkz/index_spec.ts
@@ -3,31 +3,25 @@ import * as path from 'path';
 import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 
-const collectionPath = path.join(__dirname, "../collection.json");
+const collectionPath = path.join(__dirname, '../collection.json');
 
-describe("ng-new", () => {
-  describe("[Layers]", () => {
-    it("should not create any layer folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+describe('ng-new', () => {
+  describe('[Layers]', () => {
+    it('should not create any layer folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
-      runner
-        .runSchematicAsync("ng-new", { name: "any-name" }, Tree.empty())
-        .subscribe((tree) => {
-          expect(tree.files.length).toEqual(32);
-        });
+      runner.runSchematicAsync('ng-new', { name: 'any-name' }, Tree.empty()).subscribe((tree) => {
+        expect(tree.files.length).toEqual(32);
+      });
     });
 
-    it("should create the project with domain layer folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+    it('should create the project with domain layer folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
       runner
-        .runSchematicAsync(
-          "ng-new",
-          { name: "any-name", shouldCreateDomainLayer: true },
-          Tree.empty()
-        )
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateDomainLayer: true }, Tree.empty())
         .subscribe((tree) => {
-          const domainFolder = tree.getDir("any-name/src/app/domain");
+          const domainFolder = tree.getDir('any-name/src/app/domain');
           expect(domainFolder).toBeDefined();
           expect(domainFolder.subfiles.length).toEqual(1);
 
@@ -35,18 +29,14 @@ describe("ng-new", () => {
         });
     });
 
-    describe("[Widget]", () => {
-      it("should create the project with widget layer folder", async () => {
-        const runner = new SchematicTestRunner("schematics", collectionPath);
+    describe('[Widget]', () => {
+      it('should create the project with widget layer folder', async () => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
 
         runner
-          .runSchematicAsync(
-            "ng-new",
-            { name: "any-name", shouldCreateWidgetLayer: true },
-            Tree.empty()
-          )
+          .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateWidgetLayer: true }, Tree.empty())
           .subscribe((tree) => {
-            const widgetFolder = tree.getDir("any-name/src/app/widget");
+            const widgetFolder = tree.getDir('any-name/src/app/widget');
 
             expect(widgetFolder).toBeDefined();
             expect(widgetFolder.subdirs.length).toEqual(3);
@@ -55,27 +45,19 @@ describe("ng-new", () => {
           });
       });
 
-      it("should have a sample pipe inside widget layer folder", async () => {
-        const runner = new SchematicTestRunner("schematics", collectionPath);
+      it('should have a sample pipe inside widget layer folder', async () => {
+        const runner = new SchematicTestRunner('schematics', collectionPath);
 
         runner
-          .runSchematicAsync(
-            "ng-new",
-            { name: "any-name", shouldCreateWidgetLayer: true },
-            Tree.empty()
-          )
+          .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateWidgetLayer: true }, Tree.empty())
           .subscribe((tree) => {
-            const BASE_DIRECTIVE_FOLDER_PATH = "any-name/src/app/widget/directives";
-            const sampleDirectiveFolder = tree.getDir(
-              `${BASE_DIRECTIVE_FOLDER_PATH}/sample`
-            );
+            const BASE_DIRECTIVE_FOLDER_PATH = 'any-name/src/app/widget/directives';
+            const sampleDirectiveFolder = tree.getDir(`${BASE_DIRECTIVE_FOLDER_PATH}/sample`);
 
             expect(sampleDirectiveFolder).toBeDefined();
             expect(sampleDirectiveFolder.subfiles.length).toEqual(3);
 
-            const sampleDirectiveFile = tree.get(
-              `${BASE_DIRECTIVE_FOLDER_PATH}/sample/sample.directive.ts`
-            );
+            const sampleDirectiveFile = tree.get(`${BASE_DIRECTIVE_FOLDER_PATH}/sample/sample.directive.ts`);
 
             const sampleDirectiveWidgetModule = tree.get(
               `${BASE_DIRECTIVE_FOLDER_PATH}/sample/sample.directive.module.ts`
@@ -87,17 +69,13 @@ describe("ng-new", () => {
       });
     });
 
-    it("should create the project with shared layer folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+    it('should create the project with shared layer folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
       runner
-        .runSchematicAsync(
-          "ng-new",
-          { name: "any-name", shouldCreateSharedLayer: true },
-          Tree.empty()
-        )
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateSharedLayer: true }, Tree.empty())
         .subscribe((tree) => {
-          const sharedFolder = tree.getDir("any-name/src/app/shared");
+          const sharedFolder = tree.getDir('any-name/src/app/shared');
 
           expect(sharedFolder).toBeDefined();
           expect(sharedFolder.subdirs.length).toEqual(4);
@@ -106,17 +84,13 @@ describe("ng-new", () => {
         });
     });
 
-    it("should create the project with core layer folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+    it('should create the project with core layer folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
       runner
-        .runSchematicAsync(
-          "ng-new",
-          { name: "any-name", shouldCreateCoreLayer: true },
-          Tree.empty()
-        )
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateCoreLayer: true }, Tree.empty())
         .subscribe((tree) => {
-          const coreFolder = tree.getDir("any-name/src/app/core");
+          const coreFolder = tree.getDir('any-name/src/app/core');
 
           expect(coreFolder).toBeDefined();
           expect(coreFolder.subfiles.length).toEqual(1);
@@ -125,14 +99,14 @@ describe("ng-new", () => {
         });
     });
 
-    it("should create the project with all layers folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+    it('should create the project with all layers folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
       runner
         .runSchematicAsync(
-          "ng-new",
+          'ng-new',
           {
-            name: "any-name",
+            name: 'any-name',
             shouldCreateCoreLayer: true,
             shouldCreateDomainLayer: true,
             shouldCreateSharedLayer: true,
@@ -141,10 +115,10 @@ describe("ng-new", () => {
           Tree.empty()
         )
         .subscribe((tree) => {
-          const coreFolder = tree.getDir("any-name/src/app/core");
-          const domainFolder = tree.getDir("any-name/src/app/domain");
-          const sharedFolder = tree.getDir("any-name/src/app/shared");
-          const widgetFolder = tree.getDir("any-name/src/app/widget");
+          const coreFolder = tree.getDir('any-name/src/app/core');
+          const domainFolder = tree.getDir('any-name/src/app/domain');
+          const sharedFolder = tree.getDir('any-name/src/app/shared');
+          const widgetFolder = tree.getDir('any-name/src/app/widget');
 
           expect(coreFolder).toBeDefined();
           expect(coreFolder.subfiles.length).toEqual(1);
@@ -162,49 +136,151 @@ describe("ng-new", () => {
         });
     });
 
-    it("should create the project with sass folder", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+    it('should create the project with sass folder', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
-      runner
-        .runSchematicAsync("ng-new", { name: "any-name" }, Tree.empty())
-        .subscribe((tree) => {
-          const sassFolder = tree.getDir("any-name/src/sass");
+      runner.runSchematicAsync('ng-new', { name: 'any-name' }, Tree.empty()).subscribe((tree) => {
+        const sassFolder = tree.getDir('any-name/src/sass');
 
-          expect(sassFolder).toBeDefined();
-          expect(sassFolder.subfiles.length).toEqual(5);
-          expect(tree.files.length).toEqual(32);
-        });
+        expect(sassFolder).toBeDefined();
+        expect(sassFolder.subfiles.length).toEqual(5);
+        expect(tree.files.length).toEqual(32);
+      });
     });
   });
 
-  describe("[TS Config]", () => {
-    it("should set strictPropertyInitialization to false", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+  describe('[TS Config]', () => {
+    it('should set strictPropertyInitialization to false', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
-      runner
-        .runSchematicAsync("ng-new", { name: "any-name" }, Tree.empty())
-        .subscribe((tree) => {
-          const tsConfig = tree.read("any-name/tsconfig.json");
+      runner.runSchematicAsync('ng-new', { name: 'any-name' }, Tree.empty()).subscribe((tree) => {
+        const tsConfig = tree.read('any-name/tsconfig.json');
 
-          if (!tsConfig) throw new Error("tsconfig.json not found");
-          const tsConfigJson = JSON.parse(tsConfig.toString());
+        if (!tsConfig) throw new Error('tsconfig.json not found');
+        const tsConfigJson = JSON.parse(tsConfig.toString());
 
-          expect(tsConfigJson.compilerOptions.strictPropertyInitialization).toBeFalsy();
-        });
+        expect(tsConfigJson.compilerOptions.strictPropertyInitialization).toBeFalsy();
+      });
     });
 
     it("should set baseUrl to 'src'", async () => {
-      const runner = new SchematicTestRunner("schematics", collectionPath);
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+
+      runner.runSchematicAsync('ng-new', { name: 'any-name' }, Tree.empty()).subscribe((tree) => {
+        const tsConfig = tree.read('any-name/tsconfig.json');
+
+        if (!tsConfig) throw new Error('tsconfig.json not found');
+        const tsConfigJson = JSON.parse(tsConfig.toString());
+
+        expect(tsConfigJson.compilerOptions.baseUrl).toEqual('src');
+      });
+    });
+
+    it('should have path mapping configured for core layer', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
 
       runner
-        .runSchematicAsync("ng-new", { name: "any-name" }, Tree.empty())
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateCoreLayer: true }, Tree.empty())
         .subscribe((tree) => {
-          const tsConfig = tree.read("any-name/tsconfig.json");
+          const tsConfig = tree.read('any-name/tsconfig.json');
 
-          if (!tsConfig) throw new Error("tsconfig.json not found");
+          if (!tsConfig) throw new Error('tsconfig.json not found');
           const tsConfigJson = JSON.parse(tsConfig.toString());
 
-          expect(tsConfigJson.compilerOptions.baseUrl).toEqual("src");
+          const pathKeys = Object.keys(tsConfigJson.compilerOptions.paths);
+
+          expect(pathKeys.length).toEqual(1);
+          expect(tsConfigJson.compilerOptions.paths['@app/core']).toEqual(['app/core']);
+        });
+    });
+
+    it('should have path mapping configured for domain layer', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+
+      runner
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateDomainLayer: true }, Tree.empty())
+        .subscribe((tree) => {
+          const tsConfig = tree.read('any-name/tsconfig.json');
+
+          if (!tsConfig) throw new Error('tsconfig.json not found');
+          const tsConfigJson = JSON.parse(tsConfig.toString());
+
+          const pathKeys = Object.keys(tsConfigJson.compilerOptions.paths);
+
+          expect(pathKeys.length).toEqual(1);
+          expect(tsConfigJson.compilerOptions.paths['@app/domain']).toEqual(['app/domain']);
+        });
+    });
+
+    it('should have path mapping configured for shared layer', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+
+      runner
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateSharedLayer: true }, Tree.empty())
+        .subscribe((tree) => {
+          const tsConfig = tree.read('any-name/tsconfig.json');
+
+          if (!tsConfig) throw new Error('tsconfig.json not found');
+          const tsConfigJson = JSON.parse(tsConfig.toString());
+
+          const pathKeys = Object.keys(tsConfigJson.compilerOptions.paths);
+
+          expect(pathKeys.length).toEqual(1);
+          expect(tsConfigJson.compilerOptions.paths['@app/shared']).toEqual(['app/shared']);
+        });
+    });
+
+    it('should have path mapping configured for widget layer', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+
+      runner
+        .runSchematicAsync('ng-new', { name: 'any-name', shouldCreateWidgetLayer: true }, Tree.empty())
+        .subscribe((tree) => {
+          const tsConfig = tree.read('any-name/tsconfig.json');
+
+          if (!tsConfig) throw new Error('tsconfig.json not found');
+          const tsConfigJson = JSON.parse(tsConfig.toString());
+
+          const pathKeys = Object.keys(tsConfigJson.compilerOptions.paths);
+
+          expect(pathKeys.length).toEqual(3);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/components']).toEqual(['app/widget/components']);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/directives']).toEqual(['app/widget/directives']);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/pipes']).toEqual(['app/widget/pipes']);
+        });
+    });
+
+    it('should have path mapping configured with project prefix', async () => {
+      const runner = new SchematicTestRunner('schematics', collectionPath);
+
+      runner
+        .runSchematicAsync(
+          'ng-new',
+          {
+            name: 'any-name',
+            shouldCreateCoreLayer: true,
+            shouldCreateDomainLayer: true,
+            shouldCreateSharedLayer: true,
+            shouldCreateWidgetLayer: true,
+          },
+          Tree.empty()
+        )
+        .subscribe((tree) => {
+          const tsConfig = tree.read('any-name/tsconfig.json');
+
+          if (!tsConfig) throw new Error('tsconfig.json not found');
+          const tsConfigJson = JSON.parse(tsConfig.toString());
+
+          const pathKeys = Object.keys(tsConfigJson.compilerOptions.paths);
+
+          expect(pathKeys.length).toEqual(6);
+
+          expect(tsConfigJson.compilerOptions.paths['@app/core']).toEqual(['app/core']);
+          expect(tsConfigJson.compilerOptions.paths['@app/domain']).toEqual(['app/domain']);
+          expect(tsConfigJson.compilerOptions.paths['@app/shared']).toEqual(['app/shared']);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/components']).toEqual(['app/widget/components']);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/directives']).toEqual(['app/widget/directives']);
+          expect(tsConfigJson.compilerOptions.paths['@app/widget/pipes']).toEqual(['app/widget/pipes']);
         });
     });
   });
